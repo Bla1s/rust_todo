7c6007aff16e9b42725a615cf4d7402210c51c14
Bla1s <blazej1gg@gmail.com>
2024-01-17 19:56:53 +0100

    -final changes to the rust todo app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use regex::Regex;
 use std::fmt;
+// Używamy PartialOrd oraz PartialEq aby móc porównywać kategorie
+// Clone do możliwości klonowania objektów (kategorii)
+// Debug do możliwości printowania kategorii
 #[derive(PartialOrd, PartialEq, Clone, Debug)]
 enum Category {
     Any,
@@ -7,34 +10,43 @@ enum Category {
     InProgress,
     Done,
 }
+// Implementacja Traitu dzięki któremu możemy printować kategorie jako string
 impl fmt::Display for Category {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
+// Używamy Clone do możliwości klonowania Todo
+// Debug do możliwości printowania Todo
 #[derive(Clone, Debug)]
 struct Todo {
     name: String,
     category: Category,
 }
+// Wektor zawierając wszystkie obiekty typu Todo
 struct TodoList {
     todos: Vec<Todo>,
 }
 impl TodoList {
+    // Konstruktor
     fn new() -> TodoList {
         TodoList { todos: Vec::new() }
     }
+    // Dodawanie Todo do wektora
     fn add(&mut self, todo: Todo) {
         self.todos.push(todo);
     }
+    // Usuwanie Todo z wektora
     fn remove(&mut self, index: usize) {
         self.todos.remove(index);
     }
+    // Wyświetlanie wszystkich Todo
     fn display_tasks(&self) {
         for (i, todo) in self.todos.iter().enumerate() {
             println!("{} - [{:?}] {}", i, todo.category, todo.name);
         }
     }
+    // funkcja która służy do nadawaia priorytetów kategoriom
     fn category_priority(&self, category: &Category) -> u8 {
         match category {
             Category::Any => 0,
@@ -43,24 +55,33 @@ impl TodoList {
             Category::Done => 3,
         }
     }
-
+    // Wyświetlanie wszystkich Todo posortowanych po kategoriach
     fn display_tasks_by_category(&self) {
+        // Tworzymy wektor krotek (indeks, todo) i sortujemy go po priorytecie kategorii
         let mut todos: Vec<(usize, &Todo)> = self.todos.iter().enumerate().collect();
+        // sortujemy po priorytecie kategorii
         todos.sort_by(|a, b| {
+            // porównujemy priorytety kategorii
             self.category_priority(&a.1.category)
+                // cmp zwraca Ordering który jest typem enum
                 .cmp(&self.category_priority(&b.1.category))
         });
-
+        // printujemy posortowane todo
         for (i, todo) in todos {
             println!("{} - [{:?}] {}", i, todo.category, todo.name);
         }
     }
+    // Funkcja która zwraca nam wszystkie Todo z pliku
     fn get_tasks_from_file(filename: &str) -> std::io::Result<Self> {
         let content = std::fs::read_to_string(filename)?;
         let mut todos = Vec::new();
+        // Regex do matchowania Todo z pliku
         let re = Regex::new(r"\[(.*?)\] (.*)").unwrap();
 
         for line in content.lines() {
+            // przy pomocy regexa matchujemy linie z pliku
+            // używamy captures w celu wyciągnięcia grup z regexa
+            // matchujemy kategorię oraz nazwę
             if let Some(caps) = re.captures(line) {
                 let category = match caps.get(1).map(|m| m.as_str()) {
                     Some("Any") => Category::Any,
@@ -69,6 +90,7 @@ impl TodoList {
                     Some("Done") => Category::Done,
                     _ => continue,
                 };
+                // używamy mapy żeby móc przekonwertować wartość (jeżeli jakaś jest) na String
                 let name = caps
                     .get(2)
                     .map(|m| m.as_str().to_string())
@@ -81,11 +103,11 @@ impl TodoList {
     }
     fn update_tasks_to_file(&self, filename: &str) {
         let mut content = String::new();
-
+        // zapisujemy wszystkie todo do stringa
         for todo in &self.todos {
             content.push_str(&format!("[{:?}] {}\n", todo.category, todo.name));
         }
-
+        // zapisujemy stringa do pliku
         match std::fs::write(filename, content) {
             Ok(_) => (),
             Err(e) => eprintln!("Failed to write to file: {}", e),
@@ -94,8 +116,10 @@ impl TodoList {
 }
 
 fn main() {
+    // Tworzymy nową listę todo
     let mut todo_list = TodoList::new();
     println!("Program for a simple Todo implementation");
+    // pobieramy dane z pliku
     match TodoList::get_tasks_from_file("Todos.txt") {
         Ok(todos) => todo_list = todos,
         Err(e) => eprintln!("Failed to read from file: {}", e),
